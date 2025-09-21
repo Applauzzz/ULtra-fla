abeebeae07707e299e3d9696217e6e90b4d612c6
chouyuhong <chouyuhong@bytedance.com>
2025-09-21 10:02:32 +0000

    add olmo

--- fla/models/olmo/__init__.py
+++ fla/models/olmo/__init__.py
@@ -2,12 +2,12 @@
 
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
 
-from fla.models.transformer.configuration_transformer import TransformerConfig
-from fla.models.transformer.modeling_transformer import TransformerForCausalLM, TransformerModel
+from fla.models.olmo.configuration_transformer import TransformerConfig
+from fla.models.olmo.modeling_transformer import TransformerForCausalLM, TransformerModel
 
-AutoConfig.register(TransformerConfig.model_type, TransformerConfig, exist_ok=True)
-AutoModel.register(TransformerConfig, TransformerModel, exist_ok=True)
-AutoModelForCausalLM.register(TransformerConfig, TransformerForCausalLM, exist_ok=True)
+# AutoConfig.register(TransformerConfig.model_type, TransformerConfig, exist_ok=True)
+# AutoModel.register(TransformerConfig, TransformerModel, exist_ok=True)
+# AutoModelForCausalLM.register(TransformerConfig, TransformerForCausalLM, exist_ok=True)
 
 
-__all__ = ['TransformerConfig', 'TransformerForCausalLM', 'TransformerModel']
+# __all__ = ['TransformerConfig', 'TransformerForCausalLM', 'TransformerModel']
